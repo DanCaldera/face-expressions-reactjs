4b27d832878c3744eb2cfa21530a14dd52db2bf6
Daniel Caldera <bdcaldera@gmail.com>
2020-08-26 17:42:47 -0500

    wroking

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import objectDetectionSketch from './ObjectDetectionSketch'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import P5Wrapper from 'react-p5-wrapper'
+
+export default class app extends React.Component {
+  render() {
+    return <P5Wrapper sketch={objectDetectionSketch} />
+  }
 }
-
-export default App;
